f0c650a21e5efd00b670eb46fda2f699f1335941
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-11 08:13:49 +0530

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -31,17 +31,17 @@ variable "instance_type" {
 variable "ami" {
   description = "The AMI ID to use for the EC2 instance."
   type        = string
-  default     = "ami-0abcdef1234567890" # Replace with your desired AMI ID
+  default     = "ami-084568db4383264d4"
 }
 
 variable "start_lambda_filename" {
   description = "The local filename of the start Lambda function code."
   type        = string
-  default     = "start-ec2-instance.zip"
+  default     = "./function/start-ec2-instance.zip"
 }
 
 variable "stop_lambda_filename" {
   description = "The local filename of the stop Lambda function code."
   type        = string
-  default     = "stop-ec2-instance.zip"
+  default     = "./function/stop-ec2-instance.zip"
 }
